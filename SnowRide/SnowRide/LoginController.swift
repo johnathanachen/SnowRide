033d4046946d46b88f7df30f2f9c8855e434fb3d
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-30 14:14:01 -0700

    Draft 1 for Loging UI

---
+++ SnowRide/SnowRide/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  SnowRide
+//
+//  Created by Johnathan Chen on 10/24/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
