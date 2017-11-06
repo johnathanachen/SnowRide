96fda427436595f10c8213abbc9b4adf32c60dc5
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-06 14:43:45 -0800

    fixed UI for login / register view

--- SnowRide/SnowRide/LoginController.swift
+++ SnowRide/SnowRide/LoginController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class LoginController: UIViewController  {
     
     @IBOutlet weak var usrDetails: UITableView!
     
