1954b9f5f18c19abbd63f46eff9f0f4c56cc65d7
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-08 20:25:01 -0800

    add functionality to the date picker view

--- SnowRide/PickScheduleVC.swift
+++ SnowRide/PickScheduleVC.swift
@@ -10,14 +10,71 @@ import UIKit
 
 class PickScheduleVC: UIViewController {
 
+    @IBOutlet weak var departOnTxt: UITextField!
+    @IBOutlet weak var returnOnTxt: UITextField!
+    
+    let departOnDatePicker = UIDatePicker()
+    let returnOnDatePicker = UIDatePicker()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        createDepartOnPicker()
+        createReturnOnPicker()
     }
 
+    func createDepartOnPicker() {
+        
+        // format for picker
+        departOnDatePicker.datePickerMode = .date
+        
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // bar button item
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(didPressDone))
+        toolbar.setItems([doneButton], animated: false)
+
+        departOnTxt.inputAccessoryView = toolbar
+        
+        // assigning date picker to text field
+        departOnTxt.inputView = departOnDatePicker
+        
+    }
     
+    func createReturnOnPicker() {
+        
+        // format for picker
+        returnOnDatePicker.datePickerMode = .date
+        
+        // toolbar
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // bar button item
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(didPressDone))
+        toolbar.setItems([doneButton], animated: false)
+        
+        returnOnTxt.inputAccessoryView = toolbar
+        
+        // assigning date picker to text field
+        returnOnTxt.inputView = returnOnDatePicker
+        
+    }
+    
+    @objc func didPressDone() {
+        // format date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .short
+        dateFormatter.timeStyle = .none
+        
+        if departOnTxt.isEditing {
+            departOnTxt.text = dateFormatter.string(from: departOnDatePicker.date)
+        } else if returnOnTxt.isEditing {
+            returnOnTxt.text = dateFormatter.string(from: returnOnDatePicker.date)
+        }
+        self.view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
